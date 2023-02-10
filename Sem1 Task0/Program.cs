a70a2d0e6ba9f4f3b7a117a5246b749b9a6d545f
Iwan Yudzitski <iwan201087@gmail.com>
2023-02-10 22:19:09 +0300

    Пуш задач

---
+++ Sem1 Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Считываем из консоли
+string? inputNum = Console.ReadLine ();
+// Проверяем, что данные не путые
+if (inputNum != null)
+{
+    //Парсим введенное число
+    int number = int.Parse(inputNum);
+    // находим квадрат числа
+    int outNum = (int)Math.Pow(number,2);
+    Console.WriteLine("квадрат числа: ",+outNum);
+}
