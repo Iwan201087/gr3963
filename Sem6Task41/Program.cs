141c7a002df9b272a67e8eecc0e0ebee9500c338
Iwan Yudzitski <iwan201087@gmail.com>
2023-03-06 23:56:23 +0300

    Домашняя работа к семинару №6

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//Принимаем из консоли число
+int ReadData(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+int[] InputArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadData ($"Введите  число");
+    }
+    return array;
+}
+//Метод, выводящий на печать полученный массив из введенных чисел
+void Print1DArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+//Метод, подсчитывающий количество положительных элементов введенных пользователем
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i] > 0);
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int len=ReadData ("Введите количество вводимых элементов");
+int [] array;
+array = InputArray (len);
+Print1DArray (array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,37 @@
+﻿int coefficient = 0;
+int constant = 1;
+int xCoord = 0;
+int yCoord = 1;
+int line1 = 1;
+int line2 = 2;
+// Метод для ввода данных от пользователя в вещественном формате
+double ReadData(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToDouble(Console.ReadLine() ?? "0");
+}
+//Метод для ввода параметров уравнения прямых
+double  InputLineData (int numberLine)
+{
+    double [] lineData = new double [2];
+    lineData[coefficient] = ReadData($"Введите коэффициент для х {numberLine} прямой  ");
+    lineData[constant] = ReadData($"Введите константу в для  {numberLine} прямой  ");
+    return lineData;
+}
+//Метод для нахождения координат пересечения точки пересечения прямых
+double  FindCrossPointCoords(double [] lineData1, double [] lineData2)
+{
+    double [] coord = new double [2];
+    coord[xCoord] = ((lineData1[constant]-lineData2[constant]))/((lineData1[coefficient]-lineData1[coefficient]));
+    coord[yCoord] = lineData1[constant]*coord[xCoord]+lineData1[constant];
+    return coord;
+}
+//Метод вывода координат точки пересечения прямых
+double [] ValidateLine( double lineData1, double lineData2)
+{
+    coord = FindCrossPointCoords (lineData1, lineData2);
+    Console.Write ($"Точка пересечения уравнений у= {lineData1[coefficient]}*X+ {lineData1[constant]} и у= {lineData2[coefficient]}*X+ {lineData2[constant]} имеет координаты ({coord[xCoord]},({coord[yCoord]}");
+}
+double   lineData1=InputLineData(line1);
+double  lineData2=InputLineData(line2);
+ValidateLine (xcoord, yCoord);
